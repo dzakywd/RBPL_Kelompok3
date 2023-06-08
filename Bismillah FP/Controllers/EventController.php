c2ff5ef2ed82ce5f5eb94efba5ef0f41e84710fa
Jihan Husnia U <108179160+Jihanut@users.noreply.github.com>
2023-06-08 23:46:09 +0700

    Update EventController.php

--- Bismillah FP/Controllers/EventController.php
+++ Bismillah FP/Controllers/EventController.php
@@ -15,14 +15,12 @@ class EventController extends Controller
 
     public function detailEvent($id)
     {
-
         // mengambil data event berdasarkan id yang dipilih
         $event = DB::table('event')->where('id_event',$id)->get();
         $rating = DB::table('rating_review')
         ->join('event', 'rating_review.id_event', '=', 'event.id_event')
         ->join('pengguna', 'rating_review.id_user', '=', 'pengguna.id_user')
         ->where('rating_review.id_event',$id)->get();
-        // $rating = DB::table('rating_review')->where('id_event',$id_r = $id)->get();
 
         // passing data dari event dan rating_review ke detailEVent.blade.php
         return view('detailEVent',['event' => $event, 'rating' => $rating]);
