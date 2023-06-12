f49c54380ad08005f2c2e6ab6231917ac15a3db9
dzakywd <89232924+dzakywd@users.noreply.github.com>
2023-06-12 15:09:47 +0700

    Update dashboardcopy.blade.php

--- Bismillah FP/dashboardcopy.blade.php
+++ Bismillah FP/dashboardcopy.blade.php
@@ -15,8 +15,13 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-
+    <!-- Iconify -->
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+   
     <style>
+        *{
+            font-family: 'Lexend', sans-serif;
+        }
 
         .container-fluid{
             background-color: #ECEADD;
@@ -73,7 +78,129 @@
         }
 
         #content{
+            padding: 30px;
+            position: relative;
+            overflow-y: scroll;
+            max-height: 100%;
+        }
+
+        #content::-webkit-scrollbar{
+            width:0;
+        }
+
+        #content h2{
+            /* width: 300px;
+            height: 48px; */
+            margin: 0;
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: bold;
+            font-size: 25px;
+            line-height: 48px;
+            align-items: center;
+            color: #143362;
+        }
+
+        #content h3{
+            /* width: 327px;
+            height: 23px; */
+            font-family: 'Lexend';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 15px;
+            line-height: 22px;
+            color: #5F647E;
+        }
+
+        #content #searchbar{
+            padding-right: 0;
+        }
+
+        #content #search{
+            /* width: 800px;*/
+            height: 50px;
+            /* margin-right: 150px; */
+            background: #C9DAE8;
+            border-radius: 14px;
+            border: none;
+            outline: none;
+            padding-right: 0px;
+        }
+
+        #content #search-btn{
+            width: 128px;
+            height: 50px;
+            background: #FFC68F;
+            border-radius: 14px;
+            border: none;
+            outline: none;
+            padding: 0;
+        }
+
+        #content #filter{
+            max-width: 50px;
+            padding: 0;
+            margin: 0 auto;
+        }
+
+        #content #notif{
+            max-width: 50px;
+            padding: 0;
+            margin: 0 22px 0 10px;
+        }
+
+        #content #mini-profile{
+            max-width: 50px;
+            height: 50px;
+            background-image: url("Logo Icon/Mark.svg"); /*nanti ambil dr database*/
+            background-size: cover;
+            border-radius: 100%;
+            padding:0;
+            margin-right: 15px;
+        }
+
+        #popular{
+            background-color: #D9D9D9;
+            mix-blend-mode: darken;
+            border-radius: 20px;
+            padding: 30px;
+        }
+
+        .wrapper{
+            max-width: 904px;
             
+            overflow-x: scroll;
+            overflow-y: hidden;
+            white-space: nowrap;
+        }
+
+        .wrapper::-webkit-scrollbar{
+            width:0;
+        }
+
+        #discover{
+            width:160px; 
+            background-color: #D9D9D9; 
+            mix-blend-mode: darken; 
+            border-radius: 20px; 
+            border: none;
+            outline: none;
+            display: flex;
+            display: inline-block;
+            margin-right: 10px;
+            margin-bottom: 10px;
+        }
+
+        #discover .card-img-top{
+            padding: 15px;
+        }
+
+        #discover .card-body{
+            padding:0;
+        }
+
+        #discover .card-title{
+            margin: 0 15px 10px 15px
         }
 
         #sidebar-kanan{
@@ -101,11 +228,10 @@
         }
 
         #sidebar-kanan .profilepic{
-            grid-row: 2;
             margin: auto auto;
             width: 120px;
             height: 120px;
-            background-image: url("Logo Icon/Mark.svg"); /*nanti ambil dr database*/
+            background-image: url("Logo Icon/Mark.svg"); /*nanti ambil dr database */
             background-size: cover;
             border-radius: 100%;
         }
@@ -144,24 +270,20 @@
             color: rgba(255, 255, 255, 0.77);
         }
 
-        #sidebar-kanan .login{
-            margin: 0 auto;
-            display: flex;
+        #sidebar-kanan #generate{
+            position: relative;
+            margin-left: 48px;
             width: 130px;
             height: 50px;
             background: #F3E177;
             border-radius: 14px;
         }
 
-        #sidebar-kanan .login h6{
-            margin: auto;
-        }
-
     </style>
 </head>
 <body>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row" id="overall">
             <div class="col-2.5" id="sidebar-kiri">
                 <div class="logo" id="logo">
                     <img src="Logo Icon/Logo Ipsum.svg" alt="">
@@ -231,7 +353,200 @@
             </div>
 
             <div class="col" id="content">
+                <h2>Hello, (nama)!</h2>
+                <h3>Welcome back and explore the world.</h3>
+
+                <form>
+                    <div class="row">
+                        <div class="col-7" id="searchbar">
+                            <input type="text" class="container-fluid" id="search" name="cari" placeholder="Search for your favourite destination" value="{{ old('cari') }}">
+                        </div>
+
+                        <div class="col-2" id="searchbar" action="/dashboard/results" method="GET">
+                            <button type="submit" class="search-btn" id="search-btn">
+                                Search
+                            </button>
+                        </div>
+
+                        <div class="col-1" id="filter">
+                            <!--  <iconify-icon icon="ic:outline-notifications-none"></iconify-icon> -->
+                            <iconify-icon icon="ri:equalizer-line" style="font-size: 37px; color: black; background-color: #FFC68F; border-radius: 14px; padding: 5px;"></iconify-icon>
+                        </div>
+
+                        <div class="col-1" id="notif">
+                            <iconify-icon icon="ic:outline-notifications-none" style="font-size: 37px; color: black; background-color: #FFC68F; border-radius: 100%; padding: 5px;"></iconify-icon>
+                        </div>
+
+                        <div class="col-1" id="mini-profile">
+                            <!-- <img src="Logo Icon/Mark.svg" alt=""> -->
+                        </div>
+                    </div>
+                    <!-- <div class="container-fluid">
+                        <div class="row">
+                            <div class="col" id="searchbar">
+                                <input type="text" id="search" name="cari" placeholder="Search for your favourite destination" value="{{ old('cari') }}">
+                            
+                                <button type="submit" class="search-btn" id="search-btn">
+                                    Search
+                                </button>
+                            
+                            
+                                <iconify-icon icon="ri:equalizer-line" style="font-size: 37px; color: black; background-color: #FFC68F; border-radius: 14px; padding: 5px;"></iconify-icon>
+                        
+                                <iconify-icon icon="ic:outline-notifications-none" style="font-size: 37px; color: black; background-color: #FFC68F; border-radius: 100%; padding: 5px;"></iconify-icon>
+                        
+                            </div>
+                        </div>
+                    </div> -->
+                </form>
+
+                <h2>Popular Today</h2>
+
+                <div class="container-fluid" id="popular">
+                    @foreach($event as $p)
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="Logo Icon/Poster.svg" alt="" style="height: 220px; width: 220px; margin-left:20px">
+                        </div>
+                        <div class="col-8">
+                            <h5 style="font-family: 'Poppins'; font-weight: 700; font-size: 28px; color: #143362">{{ $p->title }}</h5>
+                            <p style="font-size: 20px; vertical-align:middle; margin:10px 0">
+                                <iconify-icon icon="ion:location-outline" style="font-size: 30px; color: black; vertical-align:middle"></iconify-icon> {{ $p->event_location }}
+                            </p>
+                            <p style="font-size: 20px; vertical-align:middle; margin:0">
+                                <iconify-icon icon="ant-design:star-filled" style="font-size: 30px; color: black; vertical-align:middle"></iconify-icon> (rating)
+                            </p>
+                            <p style="color:#5F647E; margin-top: 10px">
+                            {{ $p->event_detail }}
+                            </p>
+                            <button type="button" class="btn" style="background-color: #FFC68F; border-radius: 10px ">
+                                Detail
+                            </button>
+                            <button type="button" class="btn" style="background-color: #FD7B46; border-radius: 10px; margin-left: 10px">
+                                Get a Ticket
+                            </button>
+
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+
+                <h2>Discover World</h2>
+                    <div class="wrapper">
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+                    
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+                   
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+
+                        <div class="card" id="discover">
+                            <img class="card-img-top" src="Logo Icon/Poster.svg" alt="">
+                            <div class="card-body">
+                                <h6 class="card-title">(Nama Event)</h6>
+                            </div>
+                        </div>
+                    </div>
                 
+                <!-- <div class="con" id="popular" style="width:500px, margin:0">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="Logo Icon/poster.png" width= "133px" height="136px"/>
+                        </div>
+                        <div class="text col-7">
+                            <h5 class="b">(Nama Event)</h5>
+                    </div>
+                </div> -->
+
+                <!-- <div class="card" id="bookmark">
+                    <div class="row">
+                        <div class="col-xl-1.5">
+                            <img src="Logo Icon/poster.png" width= "133px" height="136px"/>
+                        </div>
+                        <div class="text col-xl-10">
+                            <h5 class="card-title">Festival Musik Pestapora</h5>
+                            <div class="keterangan">
+                                <p> Festival Musik • Jakarta
+                                    <span class="date-time ml-5">
+                                            <iconify-icon icon="material-symbols:calendar-month-rounded" style="font-size: 24px; color: #143362; vertical-align:-5px;"></iconify-icon>
+                                        23-25 September 2022
+                                    </span>
+                                </p>
+                                <p class="card-text mt-2">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content --ini deskripsi--.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer bg-transparent mt-3 ">
+                        <a href="/" class="btn">Detail</a>
+                    </div>
+                </div> -->
+
             </div>
 
             <div class="col-2" id="sidebar-kanan">
@@ -265,14 +580,14 @@
                     <p>Dont know what to do?</p>
                 </div>
 
-                <div class="login">
-                    <h6>Generate!</h6>
-                </div>
+                <button type="button" class="btn" id="generate">
+                    Generate!
+                </button>
             </div>
         </div>
         
     </div>
-
+    {{ $event->links() }}
     <script>
     function show() {
       document.getElementById("sidebar-kanan").style.width = "300px";
