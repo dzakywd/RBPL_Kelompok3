288f88234abcff59f5a63b3e74c9a5f25786bd15
versmoon <86027195+versmoon@users.noreply.github.com>
2023-06-13 17:28:06 +0700

    TEMPLATE ESTYYYYYYY JGN SALAH BRADER

--- Bismillah FP/template.blade.php
+++ Bismillah FP/template.blade.php
@@ -1,342 +1,218 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>JalanKuy Inc.</title>
-       <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lexend&family=Poppins&display=swap" rel="stylesheet">
-<!-- jQuery library -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<style>
-*{
-    font-family: 'Lexend', sans-serif;
-    padding: 0;
-    margin: 0;
-}
-
-.container{
-    display: flex;
-    /* grid-template-columns: 20% 60% 20%; */
-    background-color: #ECEADD;
-}
-@media (min-width: 992px) {
-    .sidebar-kiri{
-        /* order: 1; */
-        width: 300px;
-        background: white;
-        border-radius: 0 25px 25px 0;
-    }
-
-    .sidebar-kiri .logo{
-        display: flex;
-    }
-
-    .sidebar-kiri .logo img{
-        margin: 25px auto;
-    }
-
-    .sidebar-kiri .list{
-        display: flex;
-        width: 240px;
-        height: 71px;
-        left: 201px;
-        top: 208px;
-        border-radius: 22px;
-        margin: 25px 25px;
-        padding-left: 10px ;
-    }
-
-    .sidebar-kiri .list:hover{
-        background: #FFC68F;
-        transition: ease-in-out 100ms;
-    }
+    <link rel="shortcut icon" href="https://cdn.dribbble.com/userupload/7740149/file/original-48a39c2d5d7d49ba6a384eea98e116f0.png?compress=1&resize=400x333" style="width=100%">
 
 
-    .sidebar-kiri .list .icon{
-        display: flex;
-        height: 40px;
-        width: 40px;
-        margin: auto 0;
-    }
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend&family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
-    .sidebar-kiri .list .menu{
-        font-family: 'Poppins';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 40px;
-        display: flex;
-        align-items: center;
-        color: #143362;
-        margin-left: 10px;
-    }
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    .content{
-        order: 2;
-        flex-grow: 10;
-        background-color: #ECEADD;;
-    }
+    <!-- Iconify -->
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
 
-    .sidebar-kanan{
-        display: grid;
-        grid-template-rows: 70% 5% 25%;
-        order: 3;
-        width: 300px;
-        height: 100vh;
-        background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://img.freepik.com/free-vector/gradient-mountain-landscape_23-2149162007.jpg?w=740&t=st=1684284565~exp=1684285165~hmac=df08a23e4806ce7c2de03875c1c4471337375643ba3815704d925e0817456dbf ");
-        background-size: cover;
-        border-radius: 25px 0 0 25px;
-    }
+    <style>
+        *{
+            font-family: 'Lexend', sans-serif;
+            font-size: 15px;
+        }
 
-    .sidebar-kanan .profilepic{
-        grid-row: 1;
-        align-items: center;
-        margin: 90px auto 0;
-        width: 120px;
-        height: 120px;
-        background-image: url("Logo Icon/profpic.svg"); /*nanti ambil dr database*/
-        background-size: cover;
-        border-radius: 100%;
-    }
+        .container-fluid{
+        }
 
-    .sidebar-kanan .intro{
-        grid-row: 2;
-        margin: auto;
-        display: flex;
-        height: 33px;
-        font-family: 'Lexend';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 16px;
-        line-height: 30px;
-        display: flex;
-        align-items: center;
-        text-align: center;
-        color: rgba(255, 255, 255, 0.77);
-    }
+        #sidebar-kiri{
+            background-color: yellow;
+            width: 250px;
+            background: white;
+            border-radius: 0 25px 25px 0;
+            position: -webkit-sticky;
+        }
 
-    .sidebar-kanan .login{
-        grid-row: 3;
-        margin: 0 auto;
-        display: flex;
-        width: 130px;
-        height: 50px;
-        background: #F3E177;
-        border-radius: 14px;
-    }
+        #sidebar-kiri .logo{
+            display: flex;
+        }
 
-    .sidebar-kanan .login p{
-        margin: auto;
-    }
-}
+        #sidebar-kiri .logo img{
+            margin: 25px auto;
+        }
 
-:root {
-    --sidbar-width: 240px;
-    --background-base: #ECEADD;
-    --biru-paling-tua: #143362;
-    --biru-tengah: #3569A5;
-    --biru-muda: #69A8DB;
-    --biru-paling-muda: #C9DAE8;
-    --kuning: #F3E177;
-    --orange-tua: #FD7B46;
-    --orange-muda:#FFC68F;
+        #sidebar-kiri .list{
+            display: flex;
+            width: 200px;
+            height: 71px;
+            left: 201px;
+            top: 208px;
+            border-radius: 22px;
+            margin: 25px 25px;
+            padding-left: 10px ;
+        }
 
-}
-body {
-    background: var(--background-base);
-    color: var(--biru-paling-tua);
-    font-family: var(--font-base), sans-serif;
-}
+        #sidebar-kiri .list:hover{
+            background: #FFC68F;
+            transition: ease-in-out 50ms;
+        }
 
-.bg-base {
-    background:  var(--background-base);
-    margin: 10px;
-    border-radius: 30px;
-    border-collapse: separate;
-    border-spacing: 15px 50px;
-}
-.white {
-    background: white;
-    margin: 10px;
-    border-radius: 30px;
-    border-collapse: separate;
-    border-spacing: 15px 50px;
-}
-.btn {
-    margin-top: 15px;
-    padding: 8px 18px;
-    font-weight: 600;
-    margin: 10px;
-    border-radius: 30px;
-    border-collapse: separate;
-    border-spacing: 15px 50px;
-    background: var(--orange-muda);
-    position: right;
-}
+        #sidebar-kiri .list .icon{
+            /* display: flex; */
+            height: 40px;
+            width: 40px;
+            margin: auto 0;
+        }
 
-.btn-brand {
-    background-color: var(--orange-muda);
-    border-color: var(--color-brand);
-    color: var(--background-base);
-}
+        #sidebar-kiri .list .menu{
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 600;
+            font-size: 15px;
+            line-height: 40px;
+            display: flex;
+            align-items: center;
+            color: #143362;
+            margin-left: 10px;
+        }
 
-.btn-brand:hover,
-.btn-brand:focus {
-    background-color: var(--biru-muda);
-    color: var(--background-base);
-    border-color: var(--biru-paling-tua);
-}
+        #content{
+            padding: 30px;
+            position: relative;
+            overflow-y: scroll;
+            max-height: 100%;
+        }
 
-/* POP UP ADMIN */
-.popup {
-width: 100%;
-height: 100vh;
-background-color: rgba(0, 0, 0, 0.3);
-position: fixed;
-top: 0;
-left: 0;
+        #content::-webkit-scrollbar{
+            width:0;
+        }
 
-opacity: 0;
+        #content h2{
+            /* width: 300px;
+            height: 48px; */
+            margin: 0;
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: bold;
+            font-size: 25px;
+            line-height: 48px;
+            align-items: center;
+            color: #143362;
+        }
 
-}
+        #content h3{
+            /* width: 327px;
+            height: 23px; */
+            font-family: 'Lexend';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 15px;
+            line-height: 22px;
+            color: #5F647E;
+        }
 
-#popup:target {
-    opacity: 1;
-    visibility: visible;
+        .icon iconify-icon{
+            font-size: 30px;
+            color:#143362;
+        }
 
-}
+        /* esty punya */
+        .list {
+            cursor: pointer;
+        }
 
-.popup-content {
-    width: 50%;
-    background-color: white;
-    box-shadow: 0 20px rgba(0, 0, 0, 0.3);
-}
+        .bg-settings {
+            background-color:  white;
+            margin: auto;
+            border-radius: 15px;
+        }
 
-.popup-img {
-    display: flex;
-    width: 100%;
-    margin-bottom: 30px;
-}
-
-
-</style>
+        .title-settings {
+            font-family: poppins;
+            font-size: 20px;
+            font-weight: 900;
+            color: #143362;
+        }
+    </style>
 </head>
 <body>
-    <div class="container">
-        <div class="sidebar-kiri fixed-top">
+    <div class="container-fluid" style="background: url('https://img.freepik.com/free-vector/gradient-mountain-landscape_23-2149162007.jpg?w=740&t=st=1684284565~exp=1684285165~hmac=df08a23e4806ce7c2de03875c1c4471337375643ba3815704d925e0817456dbf') no-repeat center fixed; background-size: cover">
+        <div class="row" id="overall">
+            <div class="col-2.5" id="sidebar-kiri">
+                <div class="logo" id="logo">
+                    <img src="https://cdn.dribbble.com/userupload/7740475/file/original-1c2b7b06e20f7bc7575aa8c140e77724.png?compress=1&resize=600x400" alt="" style="max-width: 149px; height: auto;">
+                </div>
 
-            <div class="logo">
-                <img src="Logo Icon/Logo Ipsum.svg" alt="">
+                <div class="list">
+
+                    <div class="icon">
+                        <iconify-icon icon="bx:home-alt"></iconify-icon>
+                    </div>
+
+                    <div class="menu">
+                        Dashboard
+                    </div>
+
+                </div>
+
+                <div class="list">
+                    <div class="icon" style="margin-top: 10px">
+                        <iconify-icon icon="ci:paper-plane"></iconify-icon>
+                    </div>
+
+                    <div class="menu">
+                        Discovery
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="icon">
+                        <iconify-icon icon="material-symbols:collections-bookmark-outline"></iconify-icon>
+                    </div>
+
+                    <div class="menu">
+                        Bookmark
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="icon">
+                        <iconify-icon icon="mdi:event-add"></iconify-icon>
+                    </div>
+
+                    <div class="menu">
+                        Register Event
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="icon">
+                        <iconify-icon icon="eva:settings-2-outline"></iconify-icon>
+                    </div>
+
+                    <div class="menu">
+                        Settings
+                    </div>
+                </div>
+
+                <div class="list">
+                    <div class="icon">
+                        <iconify-icon icon="ri:logout-circle-line"></iconify-icon>
+                    </div>
+
+                    <div class="menu">
+                        Logout
+                    </div>
+                </div>
             </div>
-
-            <div class="list">
-
-                <div class="icon">
-                    <img src="Logo Icon/Dashboard.svg" alt="" id="icon">
-                </div>
-
-                <div class="menu">
-                    Dashboard
-                </div>
-
-            </div>
-
-            <div class="list">
-
-                <div class="icon">
-                    <img src="Logo Icon/Discovery.svg" alt="">
-                </div>
-
-                <div class="menu">
-                    Discovery
-                </div>
-
-            </div>
-
-            <div class="list">
-
-                <div class="icon">
-                    <img src="Logo Icon/Bookmark.svg" alt="">
-                </div>
-
-                <div class="menu">
-                    Bookmark
-                </div>
-
-            </div>
-
-            <div class="list">
-
-                <div class="icon">
-                    <img src="Logo Icon/Notification.svg" alt="">
-                </div>
-
-                <div class="menu">
-                    Notification
-                </div>
-
-            </div>
-
-            <div class="list">
-
-                <div class="icon">
-                    <img src="Logo Icon/Setting.svg" alt="">
-                </div>
-
-                <div class="menu">
-                    Settings
-                </div>
-
+            <div class="col" id="content">
+                @yield('body')
             </div>
         </div>
-
-        <div class="content">
-            <p>tengah</p>
-        </div>
-
-        <div class="sidebar-kanan fixed fluid-right">
-            <div class="tombol">
-
-            </div>
-            <div class="profilepic">
-                <img src="" alt="">
-            </div>
-            <div class="name">
-
-            </div>
-            <div class="status">
-
-            </div>
-            <div class="intro">
-                <p>Login to explore more!</p>
-            </div>
-            <div class="login">
-                <p>Sign In</p>
-            </div>
-        </div>
-
-	@yield('atas')
-
-	<br>
-	<br>
-
-	@yield('bawah')
-    <br>
-    <br>
-</div>
+    </div>
 </body>
 </html>
