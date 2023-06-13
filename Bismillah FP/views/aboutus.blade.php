4aad2894d9c276d4487dfbe35b802932b0a5beab
versmoon <86027195+versmoon@users.noreply.github.com>
2023-06-13 16:26:25 +0700

    Update aboutus.blade.php

--- Bismillah FP/views/aboutus.blade.php
+++ Bismillah FP/views/aboutus.blade.php
@@ -1,60 +1,39 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Report a Bug</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>JalanKuy Inc.</title>
-       <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <link href="https://fonts.googleapis.com/css2?family=Lexend&family=Poppins&display=swap" rel="stylesheet">
-    <!-- jQuery library -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-
-    <!-- Popper JS -->
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <style></style>
-</head>
-
-
-<body style="background: url('https://img.freepik.com/free-vector/gradient-mountain-landscape_23-2149162007.jpg?w=740&t=st=1684284565~exp=1684285165~hmac=df08a23e4806ce7c2de03875c1c4471337375643ba3815704d925e0817456dbf') no-repeat center fixed; background-size: cover">
+@extends('template')
+@section('body')
     <div class="container align-items-center display-flex" style="margin-top: 30px;">
         <div class="row" style="margin-bottom: 20px;">
-            <div class="col col-lg-10 height-100 d-inline-block" style="background-color:  #eceadd; margin: auto; border-radius: 15px;">
-                <h5 style="margin-top: 10px; margin-bottom: 10px;">Settings</h5>
+            <div class="col col-lg-11 height-100 d-inline-block" style="background-color: white; margin: auto; border-radius: 15px;">
+                <h5  class="title-settings" style="margin-top: 10px; margin-bottom: 10px;">Settings</h5>
             </div>
-            <div class="col-md-2 sm-2 justify-content-center">
-                <i class="fa fa-bell-o w-" aria-hidden="true"></i>
+            <div class="col-md-1 sm-2 justify-content-center">
+                <iconify-icon icon="ic:outline-notifications-none" style="font-size: 30px; color: #143362; background-color: #FFC68F; border-radius: 100%; padding: 5px;"></iconify-icon>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-12 justify-content-center" style="background-color:  #eceadd; margin: auto; border-radius: 15px;">
+            <div class="col-lg-12 justify-content-center bg-settings">
                 <div class="container" style="margin-top: 20px; margin-bottom: 70px;">
                     <div class="row">
                         <div class="col-lg-1 md-2">
-                            <a href="/settings" class="button"><img src="https://api.iconify.design/ion/arrow-back-circle.svg" style="width:34px; height:34px; color: #143362;">
+                            <a href="/settings" class="button"><a href="/settings" class="button"><iconify-icon icon="emojione-monotone:left-arrow" style="font-size: 34px; color: #143362; opacity: 70%;"></iconify-icon>
                             </a>
                         </div>
                         <div class="col-lg-10 md-10">
                             <div class="row">
-                                <input type = "hidden" name = "_token" value = "<?php echo csrf_token() ?>">
-                                <h4 style="margin-bottom: 20px;">Contact and About Us</h4>
+                                <h5  class="title-settings" style="margin-bottom: 20px;">Contact and About Us</h5>
                             </div>
-                            <p>With the JalanKuy Website, users can do random generators when they have free time to find activities or activities they want to do. In addition, users can choose according to the location and category of activities they want to do. Ratings of attractions and restaurants will be displayed. This can be taken into consideration as well as an attraction for users when choosing activities. The results of the users' search data will be in the form of a list view, depending on what criteria are included in the search and filter features.</p>
-                            <p class="font-weight-bold">@jalankuy</p>
-                            <p class="font-weight-bold">jalankuy@gmail.com</p>
+                            <p class="text-justify" style="color: #666666">With the JalanKuy Website, users can do random generators when they have free time to find activities or activities they want to do. In addition, users can choose according to the location and category of activities they want to do. Ratings of attractions and restaurants will be displayed. This can be taken into consideration as well as an attraction for users when choosing activities. The results of the users' search data will be in the form of a list view, depending on what criteria are included in the search and filter features.</p>
+                            <table class="table table-borderless" style="width: 100%;">
+                                <tbody>
+                                    <tr>
+                                        <td style="width: 5%;"><iconify-icon icon="ri:instagram-fill" style="font-size: 30px; color: #545454;"></iconify-icon></td>
+                                        <td><p class="font-weight-bold" style="color: #666666">@jalankuy</p></td>
+                                    </tr>
+                                    <tr>
+                                        <td style="width: 5%;"><iconify-icon icon="ic:round-email" style="font-size: 30px; color: #545454;"></iconify-icon></td>
+                                        <td><p class="font-weight-bold" style="color: #666666">jalankuy@gmail.com</p></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                         <div class="col-lg-1"></div>
                     </div>
@@ -62,5 +41,4 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+@endsection
