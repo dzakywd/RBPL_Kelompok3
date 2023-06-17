fce9ff2fdb0bf3cc401c6a06eb9ebf366af06f82
annisamufidatun1 <126948784+annisamufidatun1@users.noreply.github.com>
2023-06-17 17:23:56 +0700

    untuk esty <3

---
+++ Bismillah FP/views/errorpage.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>JalanKuy Inc.</title>
+       <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+    {{-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> --}}
+    {{-- <link href="https://fonts.googleapis.com/css2?family=Lexend&family=Poppins&display=swap" rel="stylesheet"> --}}
+    <!-- jQuery library -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- Iconify -->
+    <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    {{-- <link href="https://fonts.googleapis.com/css2?family=Lexend&family=Poppins&display=swap" rel="stylesheet"> --}}
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600;700;800;900&family=Poppins:wght@400;500;600;700;800;900&display=swap');
+
+    body{
+        background: #ECEADD;
+    }
+    .konten{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+    #satu{
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 140px;
+        text-align: center;
+        color: #143362;
+    }
+    #dua{
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 140px;
+        /* display: flex; */
+        text-align: center;
+        color: #FD7B46;
+    }
+    #tiga{
+        font-family: 'Lexend';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 30px;
+        line-height: 38px;
+        display: flex;
+        text-align: center;
+        text-transform: lowercase;
+        color: #666666;
+    }
+    </style>
+</head>
+<body>
+    <a href="/" id="back" class="button ml-5 mt-5">
+    <iconify-icon icon="ion:arrow-back-circle" style="font-size: 60px; color: rgba(20, 51, 98, 0.7);; vertical-align:0px;"></iconify-icon>
+    </a>
+
+    <div class="konten">
+        <p id="satu"> EROR </p>
+        <p id="dua"> 404 </p>
+        <p id="satu"> PAGE NOT FOUND </p>
+        <p id="tiga">The page your are looking for doesn’t exist or some other error occured, go back to dashboard page</p>
+    </div>
+
+</body>
+</html>
